4d0403a4ea6e7abda49e58673ca62b1991f6dfbd
Lina ben aissa <100792366+Lina1403@users.noreply.github.com>
2022-03-11 11:22:26 +0100

    Update fonction.c

--- fonction.c
+++ fonction.c
@@ -1,8 +1,29 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include "minimap.h"
+int main(void)
+{
+    // declaration 
+    int a =   ;
+    int width =   ;
+    int height =  ;
+    
+
+
+  
+ 
+  
+  
+
 void background_load(SDL_Surface *image,char nom [] ,SDL_Rect pos1)
 {
 image  = IMG_Load(nom);
-pos1.x = 0 ;
-pos1.y=0;
+pos1.x =  ;
+pos1.y= ;
 pos1.h=image->h ;
 pos1.w=image->w ;
 
