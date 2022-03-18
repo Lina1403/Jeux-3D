48cd8a706df016e39d934948a16ad5b161bdae46
Lina ben aissa <100792366+Lina1403@users.noreply.github.com>
2022-03-18 11:35:58 +0100

    Update minimap.h

--- minimap.h
+++ minimap.h
@@ -1,30 +1,51 @@
 
 
+#ifndef MINIMAP_H_INCLUDED
+#define MINIMAP_H_INCLUDED
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "/usr/include/SDL/SDL.h"
 #include </usr/include/SDL/SDL_image.h>
 #include <string.h>
 
-struct minimap
-{
-	SDL_Surface* mini_map;
-	SDL_Rect pos_minimap;
-	SDL_Surface* mini_perso;
-	SDL_Rect pos_miniperso;
-	
-	
-};
-typedef struct minimap minimap; 
+ typedef struct {
+SDL_Surface *map ;// Surface du map //
+SDL_Rect map_pos;// position du map //
+
+SDL_Surface *mini_perso;// Surface du mini perso//
+SDL_Rect pos_perso_map;// position du   mini perso//
+
+SDL_Surface *mini_entite;//Surface du mini entite //
+SDL_Rect pos_entite_map; // position du  mini entite   //
 
 
-void init_minimap(minimap* mp);
-void dep_mini_perso (SDL_Rect* pos_perso,minimap m , SDL_Rect camera,int redimensionnement,int sens);
-void blit_minimap( minimap mp,SDL_Surface *screen);
-void free_minimap(minimap* mp);
-void sauvgarder (int score,char nomjoueur[],char nomfichier[]);
-void meilleur (int *score,char nomjoueur[],char nomfichier[]);
+
+}map ;
+
+
+
+
+
+void init_map (map *m,SDL_Surface *screen);
+void perso_map(map *m, perso *p)//void dep_mini_perso (SDL_Rect* pos_perso,minimap m , SDL_Rect camera,int redimensionnement);
+void entite_map(map *m, entite *e)
+void affiche_map(map *m , SDL_Surface *screen, entite *e);
+void mise_a_jour_map(map *m , SDL_Surface *screen, perso *p, entite *e)
+void free_minimap(minimap* m);//void afficher (minimap m, SDL_Surface * screen);
+/*void sauvgarder (int score,char nomjoueur[],char nomfichier[]);
+void meilleur (int *score,char nomjoueur[],char nomfichier[]);*/
 #endif
+
+
+
+
+#endif // MINIMAP_H_INCLUDED
+
+
 
 
 
